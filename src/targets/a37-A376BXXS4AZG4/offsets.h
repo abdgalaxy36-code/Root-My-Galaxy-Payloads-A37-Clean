f49bc243e213cc47f30794dcea1ad069c2606fa7
abdgalaxy36-code <abdgalaxy36@gmail.com>
2026-08-08 14:24:35 +0300

    Update offsets for A37-A376BXXS4AZG4

--- src/targets/a37-A376BXXS4AZG4/offsets.h
+++ src/targets/a37-A376BXXS4AZG4/offsets.h
@@ -1,13 +1,19 @@
-#define KERNEL_BASE 0xffffffc008000000
-#define ASHMEM_FOPS_OFF 0x13b6548
-#define KMALLOC_CACHES_OFF 0x177cb38
-#define SECURITY_HOOK_HEADS_OFF 0x177cff8
-#define SYSTEM_UNBOUND_WQ_OFF 0x229ae60
-#define LOGGERS_OFF 0x22a2928
-#define NFULNL_LOGGER_OFF 0x22a29e0
-#define INIT_TASK_OFF 0x22af700
-#define RANDOM_TABLE_OFF 0x23ede30
-#define ASHMEM_MISCS_OFF 0x24337e0
-#define ROOT_TASK_GROUP_OFF 0x24c3d40
-#define SELINUX_STATE_OFF 0x25982a0
-#define SYSCTL_BOOTID_OFF 0x267af58
+#ifndef OFFSETS_H
+#define OFFSETS_H
+
+#define KERNEL_BASE               0xffffffc008000000
+
+#define ASHMEM_FOPS_OFF           0x013b6548
+#define KMALLOC_CACHES_OFF        0x0177cb38
+#define SECURITY_HOOK_HEADS_OFF   0x0177cff8
+#define SYSTEM_UNBOUND_WQ_OFF     0x0229ae60
+#define LOGGERS_OFF               0x022a2928
+#define NFULNL_LOGGER_OFF         0x022a29e0
+#define INIT_TASK_OFF             0x022af700
+#define RANDOM_TABLE_OFF          0x023ede30
+#define ASHMEM_MISCS_OFF          0x024337e0
+#define ROOT_TASK_GROUP_OFF       0x024c3d40
+#define SELINUX_STATE_OFF         0x025982a0
+#define SYSCTL_BOOTID_OFF         0x0267af58
+
+#endif
